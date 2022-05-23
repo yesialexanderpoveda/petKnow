e54fb862edcb690a3798003431e6320731962e0c
yapy <povedayesid002@gmail.com>
2022-05-23 15:23:54 -0500

    animations

--- src/app/modules/home/components/home/animations.ts
+++ src/app/modules/home/components/home/animations.ts
@@ -15,61 +15,14 @@ export const animationAbrowLeft = trigger('animationAyeBrowLeft', [
   //....
 
   transition('void => *', [
-    animate('3s', keyframes([
-      style({ transform: 'rotate(-28deg) translate(-4px, -10x) translateX(-20px)'}),
-      style({ transform: 'rotate(-20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(-12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate('2s', keyframes([
-      style({ transform: 'rotate(-28deg) translate(-4px, -10x) translateX(-20px)'}),
-      style({ transform: 'rotate(-20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(-12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate('3s', keyframes([
-      style({ transform: 'rotate(-28deg) translate(-4px, -10x) translateX(-20px)'}),
-      style({ transform: 'rotate(-20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(-12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate('3s', keyframes([
-      style({ transform: 'rotate(-28deg) translate(-4px, -10x) translateX(-20px)'}),
-      style({ transform: 'rotate(-20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(-12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ]))
+  
 ])
 
 ])
 export const animationAbrowRight = trigger('animationAyeBrowRight', [
 
   transition('void => *', [
-    animate("3s", keyframes([
-      style({ transform: 'rotate(28deg) translate(-4px, -9x) translateX(20px)'}),
-      style({ transform: 'rotate(20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate("2s", keyframes([
-      style({ transform: 'rotate(28deg) translate(-4px, -9x) translateX(20px)'}),
-      style({ transform: 'rotate(20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate("4s", keyframes([
-      style({ transform: 'rotate(28deg) translate(-4px, -9x) translateX(20px)'}),
-      style({ transform: 'rotate(20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ])),
-    animate("3s", keyframes([
-      style({ transform: 'rotate(28deg) translate(-4px, -9x) translateX(20px)'}),
-      style({ transform: 'rotate(20deg) translate(-4px, -8px) ' }),
-      style({ transform: 'rotate(12deg) translate(-4px, -5px)' }),
-      style({ transform: 'rotate(0deg) translate(-4px, 0px) translateX(0px)' })
-    ]))
- 
+
 ])
 
 ])
@@ -79,6 +32,14 @@ export const animationAbrowRight = trigger('animationAyeBrowRight', [
 export const animationDog = trigger("animationDog", [
   transition('void => *', [
   animate('3s', keyframes([
+    style({ transform: 'scale(1,1) translateY(0)' }),
+    style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
+    style({ transform: 'scale(0.9, 1.1) translateY(-100px)' }),
+    style({ transform: 'scale(1.05, 0.95) translateY(0)' }),
+    style({ transform: 'scale(1,1) translateY(-7px)' }),
+    style({ transform: 'scale(1,1) translateY(0)' }),
+  ])),
+  animate('2s', keyframes([
     style({ transform: 'scale(1,1) translateY(0)' }),
     style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
     style({ transform: 'scale(0.9, 1.1) translateY(-100px)' }),
@@ -114,6 +75,16 @@ export const animationCat = trigger("animationCat", [
         
           ])),
           animate('2s', keyframes([
+      
+            style({ transform: 'scale(1,1) translateY(0)' }),
+            style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
+            style({ transform: 'scale(0.9, 1.1) translateY(-100px)' }),
+            style({ transform: 'scale(1.05, 0.95) translateY(0)' }),
+            style({ transform: 'scale(1,1) translateY(-7px)' }),
+            style({ transform: 'scale(1,1) translateY(0)' }),
+        
+          ])),
+          animate('3s', keyframes([
       
             style({ transform: 'scale(1,1) translateY(0)' }),
             style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
